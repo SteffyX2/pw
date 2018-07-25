c73758a6f934a80ceedc4e599f52ded2dd2fda6d
SteffyX2 <stefansantovac02@gmail.com>
2018-07-26 01:51:40 +0200

    Update title.js

--- js/title.js
+++ js/title.js
@@ -1,20 +1,20 @@
 var message = new Array();
-message[0] =  "[D]";
-message[1] =  "[DE]";
-message[2] =  "[DEE]";
-message[3] =  "[DEEF]";
-message[4] =  "[DEEF.]";
-message[5] =  "[DEEF.P]";
-message[6] =  "[DEEF.PW]";
-message[7] =  "[ur mom gay]";
-message[8] =  "[DEEF.PW]";
-message[9] =  "[DEEF.P]";
-message[10] = "[DEEF.]";
-message[11] = "[DEEF]";
-message[12] = "[DEE]";
-message[13] = "[DE]";
-message[14] = "[D]";
-message[15] = "[OwO]";
+message[0] =  "[S]";
+message[1] =  "[ S]";
+message[2] =  "[  S]";
+message[3] =  "[   S]";
+message[4] =  "[    S]";
+message[5] =  "[     S]";
+message[6] =  "[      S]";
+message[7] =  "[       S]";
+message[8] =  "[      S]";
+message[9] =  "[     S]";
+message[10] = "[    S]";
+message[11] = "[   S]";
+message[12] = "[  S]";
+message[13] = "[ S]";
+message[14] = "[S]";
+message[15] = "[1337]";
 var reps = 2;
 var speed = 50; var p = message.length;
 var T = "";
@@ -25,29 +25,6 @@ var sT = null;
 if (reps < 1) reps = 1;
 function doTheThing() {
 T = message[mC];
-A();
-}
-function A() {
-s++;
-if (s > 15) { s = 1;}
-if (s == 1)  { document.title = '(!@_@)> '+T+' '; }
-if (s == 2)  { document.title = '(!#_#)> '+T+' '; }
-if (s == 3)  { document.title = '(!^_^)> '+T+' '; }
-if (s == 4)  { document.title = '(!O_O)> '+T+' '; }
-if (s == 5)  { document.title = '(!U_U)> '+T+' '; }
-if (s == 6)  { document.title = '(!W_W)> '+T+' '; }
-if (s == 7)  { document.title = '(!T_T)> '+T+' '; }
-if (s == 8)  { document.title = '(!Z_Z)> '+T+' '; }
-if (s == 9)  { document.title = '(!X_X)> '+T+' '; }
-if (s == 10) { document.title = '(!R_R)> '+T+' '; }
-if (s == 11) { document.title = '(!K_K)> '+T+' '; }
-if (s == 12) { document.title = '(!H_H)> '+T+' '; }
-if (s == 13) { document.title = '(!V_V)> '+T+' '; }
-if (s == 14) { document.title = '(!N_N)> '+T+' '; }
-if (s == 15) { document.title = '(!Y_Y)> '+T+' '; }
-if (C < (15 * reps)) {
-sT = setTimeout("A()", speed);
-C++;
 }
 else {
 C = 0;
