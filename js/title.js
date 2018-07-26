dec7d8fa812cd3f6ac887d409c308ebdabdcc6a9
SteffyX2 <stefansantovac02@gmail.com>
2018-07-26 02:34:30 +0200

    Update title.js

--- js/title.js
+++ js/title.js
@@ -1,19 +1,19 @@
 var message = new Array();
-message[0] =  "[S-------]";
-message[1] =  "[-S------]";
-message[2] =  "[--S-----]";
-message[3] =  "[---S----]";
-message[4] =  "[----S---]";
-message[5] =  "[-----S--]";
-message[6] =  "[------S-]";
-message[7] =  "[-------S]";
-message[8] =  "[------S-]";
-message[9] =  "[-----S--]";
-message[10] = "[----S---]";
-message[11] = "[---S----]";
-message[12] = "[--S-----]";
-message[13] = "[-S------]";
-message[14] = "[S-------]";
+message[0] =  "[S]";
+message[1] =  "[ST]";
+message[2] =  "[STE]";
+message[3] =  "[STEF]";
+message[4] =  "[STEFF]";
+message[5] =  "[STEFFY]";
+message[6] =  "[STEFFYX]";
+message[7] =  "[STEFFYX.PW]";
+message[8] =  "[STEFFYX]";
+message[9] =  "[STEFFY]";
+message[10] = "[STEFF]";
+message[11] = "[STEF]";
+message[12] = "[STE]";
+message[13] = "[ST]";
+message[14] = "[S]";
 message[15] = "[1337]";
 var reps = 2;
 var speed = 50; var p = message.length;
@@ -30,21 +30,21 @@ A();
 function A() {
 s++;
 if (s > 15) { s = 1;}
-if (s == 1)  { document.title = '(!@_@)> '+T+' '; }
-if (s == 2)  { document.title = '(!#_#)> '+T+' '; }
-if (s == 3)  { document.title = '(!^_^)> '+T+' '; }
-if (s == 4)  { document.title = '(!O_O)> '+T+' '; }
-if (s == 5)  { document.title = '(!U_U)> '+T+' '; }
-if (s == 6)  { document.title = '(!W_W)> '+T+' '; }
-if (s == 7)  { document.title = '(!T_T)> '+T+' '; }
-if (s == 8)  { document.title = '(!Z_Z)> '+T+' '; }
-if (s == 9)  { document.title = '(!X_X)> '+T+' '; }
-if (s == 10) { document.title = '(!R_R)> '+T+' '; }
-if (s == 11) { document.title = '(!K_K)> '+T+' '; }
-if (s == 12) { document.title = '(!H_H)> '+T+' '; }
-if (s == 13) { document.title = '(!V_V)> '+T+' '; }
-if (s == 14) { document.title = '(!N_N)> '+T+' '; }
-if (s == 15) { document.title = '(!Y_Y)> '+T+' '; }
+if (s == 1)  { document.title = 'S '+T+' '; }
+if (s == 2)  { document.title = 'S '+T+' '; }
+if (s == 3)  { document.title = '  '+T+' '; }
+if (s == 4)  { document.title = '  '+T+' '; }
+if (s == 5)  { document.title = 'S '+T+' '; }
+if (s == 6)  { document.title = 'S '+T+' '; }
+if (s == 7)  { document.title = '  '+T+' '; }
+if (s == 8)  { document.title = '  '+T+' '; }
+if (s == 9)  { document.title = 'S '+T+' '; }
+if (s == 10) { document.title = 'S '+T+' '; }
+if (s == 11) { document.title = '  '+T+' '; }
+if (s == 12) { document.title = '  '+T+' '; }
+if (s == 13) { document.title = 'S '+T+' '; }
+if (s == 14) { document.title = 'S '+T+' '; }
+if (s == 15) { document.title = '  '+T+' '; }
 if (C < (15 * reps)) {
 sT = setTimeout("A()", speed);
 C++;
