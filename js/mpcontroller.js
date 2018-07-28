15973fbdbecb7f1e5bb0c74204d7a72bbe7041c8
SteffyX2 <28824635+SteffyX2@users.noreply.github.com>
2018-07-28 22:14:16 +0200

    Update mpcontroller.js

--- js/mpcontroller.js
+++ js/mpcontroller.js
@@ -4,3 +4,38 @@ function SetVolume(val)
         player.muted = false;
         player.volume = val / 100;
    	}
+
+var inputRange = document.getElementsByClassName('range')[0],
+    maxValue = 100, // the higher the smoother when dragging
+    currValue, rafID;
+
+// set min/max value
+inputRange.min = 0;
+inputRange.max = maxValue;
+
+// move gradient
+inputRange.addEventListener('input', function() {
+    //Change slide thumb color on way up
+    if (this.value > 20) {
+        inputRange.classList.add('ltpurple');
+    }
+    if (this.value > 40) {
+        inputRange.classList.add('purple');
+    }
+    if (this.value > 60) {
+        inputRange.classList.add('pink');
+    }
+
+    //Change slide thumb color on way down
+    if (this.value < 20) {
+        inputRange.classList.remove('ltpurple');
+    }
+    if (this.value < 40) {
+        inputRange.classList.remove('purple');
+    }
+    if (this.value < 60) {
+        inputRange.classList.remove('pink');
+    }
+});
+
+//xx Eric Grucza
