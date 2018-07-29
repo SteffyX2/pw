214c994c530a8fa35877ef2b0ddd5eda586aa1cb
SteffyX2 <28824635+SteffyX2@users.noreply.github.com>
2018-07-29 03:09:07 +0200

    Update mpcontroller.js

--- js/mpcontroller.js
+++ js/mpcontroller.js
@@ -6,7 +6,7 @@ function SetVolume(val)
    	}
 
 var inputRange = document.getElementsByClassName('range')[0],
-var texter = document.getElementsByClassName('text');
+//var texter = document.getElementsByClassName('text');
     maxValue = 100, // the higher the smoother when dragging
     currValue, rafID;
 
@@ -19,31 +19,31 @@ inputRange.addEventListener('input', function() {
     //Change slide thumb color on way up
     if (this.value > 20) {
         inputRange.classList.add('ltpurple');
-        texter.classList.add('ltpurple');
+        //texter.classList.add('ltpurple');
     }
     if (this.value > 40) {
         inputRange.classList.add('purple');
-        texter.classList.add('purple');
+       // texter.classList.add('purple');
 
     }
     if (this.value > 60) {
         inputRange.classList.add('pink');
-	texter.classList.add('pink');   
+	//texter.classList.add('pink');   
     }
 
     //Change slide thumb color on way down
     if (this.value < 20) {
         inputRange.classList.remove('ltpurple');
-        texter.classList.remove('ltpurple');
+      //  texter.classList.remove('ltpurple');
     
     }
     if (this.value < 40) {
         inputRange.classList.remove('purple');
-	texter.classList.remove('purple');    
+	//texter.classList.remove('purple');    
     }
     if (this.value < 60) {
         inputRange.classList.remove('pink');
-	texter.classList.remove('pink');    
+	//texter.classList.remove('pink');    
     }
 });
 
