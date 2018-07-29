9152199735d5f44153e0136b5a8056f778e5e123
SteffyX2 <28824635+SteffyX2@users.noreply.github.com>
2018-07-29 17:43:27 +0200

    Update mpcontroller.js

--- js/mpcontroller.js
+++ js/mpcontroller.js
@@ -2,7 +2,7 @@ function SetVolume(val)
 	{
        	var player = document.getElementById('player');
         player.muted = false;
-        player.volume = val / 100;
+        player.volume = 100 - (val / 100);
    	}
 
 
