858ac2dbadf78a2aa5169e77e60a944bd7fccc8f
SteffyX2 <28824635+SteffyX2@users.noreply.github.com>
2018-08-02 16:29:07 +0200

    Update music.js

--- js/music.js
+++ js/music.js
@@ -1,9 +1,12 @@
 			var videoSource = new Array();
-			videoSource[0]="css/files/same.mp4";
-			videoSource[1]="css/files/steppa.mp4";
-			videoSource[2]="css/files/lostit.mp4";
-			videoSource[3]="css/files/dejavu.mp4";	
-			videoSource[4]="css/files/switch.mp4";
+			videoSource[0]="css/files/lord.mp4";
+			videoSource[1]="css/files/vade.mp4";
+			videoSource[2]="css/files/warn.mp4";
+			videoSource[3]="css/files/same.mp4";
+			videoSource[4]="css/files/steppa.mp4";
+			videoSource[5]="css/files/lostit.mp4";
+			videoSource[6]="css/files/dejavu.mp4";	
+			videoSource[7]="css/files/switch.mp4";
 			var videoCount = videoSource.length;
 			document.getElementById("player").setAttribute("src",videoSource[0]);
 			document.getElementById("player").play();
